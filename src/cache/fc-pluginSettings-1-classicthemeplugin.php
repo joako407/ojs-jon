457cc0abed88329a0980db30b936fa3add02449e
Jon <joako407@gmail.com>
2022-03-05 10:31:34 +0800

    Remove cache from source control

--- src/cache/fc-pluginSettings-1-classicthemeplugin.php
+++
@@ -1,5 +0,0 @@
-<?php return array (
-  'enabled' => true,
-  'journalSummary' => '1',
-  'primaryColor' => '#ffd120',
-);
--- src/cache/fc-pluginSettings-2-classicthemeplugin.php
+++
@@ -1,4 +0,0 @@
-<?php return array (
-  'enabled' => true,
-  'primaryColor' => '#ffd120',
-);
